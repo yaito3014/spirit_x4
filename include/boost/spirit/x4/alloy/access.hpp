828e1e76fafd5551f4358bd24605297a40cf4068
yaito3014 <ykakeyama3014@gmail.com>
2025-09-18 22:02:28 +0900

    Add copyright

--- include/boost/spirit/x4/alloy/access.hpp
+++ include/boost/spirit/x4/alloy/access.hpp
@@ -1,3 +1,9 @@
+/*=============================================================================
+    Copyright (c) 2025 Yaito Kakeyama
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
 #ifndef BOOST_SPIRIT_X4_ALLOY_ACCESS_HPP
 #define BOOST_SPIRIT_X4_ALLOY_ACCESS_HPP
 
