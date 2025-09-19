68941ce45b684c429ff424420ecaa39489be4177
yaito3014 <ykakeyama3014@gmail.com>
2025-09-19 18:35:58 +0900

    Move license header

--- include/boost/spirit/alloy/detail/combine_ref.hpp
+++ include/boost/spirit/alloy/detail/combine_ref.hpp
@@ -1,11 +1,12 @@
+#ifndef BOOST_SPIRIT_ALLOY_DETAIL_COMBINE_REF_HPP
+#define BOOST_SPIRIT_ALLOY_DETAIL_COMBINE_REF_HPP
+
 /*=============================================================================
     Copyright (c) 2025 Yaito Kakeyama
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
-#ifndef BOOST_SPIRIT_ALLOY_DETAIL_COMBINE_REF_HPP
-#define BOOST_SPIRIT_ALLOY_DETAIL_COMBINE_REF_HPP
 
 #include <type_traits>
 
--- include/boost/spirit/alloy/detail/forward_like_t.hpp
+++ include/boost/spirit/alloy/detail/forward_like_t.hpp
@@ -1,11 +1,12 @@
+#ifndef BOOST_SPIRIT_ALLOY_DETAIL_FORWARD_LIKE_T_HPP
+#define BOOST_SPIRIT_ALLOY_DETAIL_FORWARD_LIKE_T_HPP
+
 /*=============================================================================
     Copyright (c) 2025 Yaito Kakeyama
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
-#ifndef BOOST_SPIRIT_ALLOY_DETAIL_FORWARD_LIKE_T_HPP
-#define BOOST_SPIRIT_ALLOY_DETAIL_FORWARD_LIKE_T_HPP
 
 #include <utility>
 
