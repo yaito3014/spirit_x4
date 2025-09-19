4e869d747451b2cf379d1c1726ff5c8c8254c60f
yaito3014 <ykakeyama3014@gmail.com>
2025-09-19 18:30:26 +0900

    Rename include guards

--- include/boost/spirit/alloy/detail/integer_seq_transform.hpp
+++ include/boost/spirit/alloy/detail/integer_seq_transform.hpp
@@ -4,8 +4,8 @@
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
-#ifndef BOOST_SPIRIT_X4_ALLOY_DETAIL_INTEGER_SEQ_TRANSFORM_HPP
-#define BOOST_SPIRIT_X4_ALLOY_DETAIL_INTEGER_SEQ_TRANSFORM_HPP
+#ifndef BOOST_SPIRIT_ALLOY_DETAIL_INTEGER_SEQ_TRANSFORM_HPP
+#define BOOST_SPIRIT_ALLOY_DETAIL_INTEGER_SEQ_TRANSFORM_HPP
 
 #include <boost/spirit/alloy/non_type_list.hpp>
 
