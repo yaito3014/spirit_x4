fed0aded75481c54e91c13a21a8d65ca85855976
yaito3014 <ykakeyama3014@gmail.com>
2025-09-22 12:46:50 +0900

    Change coding style part 1

--- include/boost/spirit/alloy/access.hpp
+++ include/boost/spirit/alloy/access.hpp
@@ -16,44 +16,45 @@
 
 #include <cstddef>
 
-namespace boost::spirit::alloy
+namespace boost::spirit::alloy {
+
+template<class T>
+struct adaptor;
+
+template<class T>
+concept TupleLike = detail::NonTypeList<typename adaptor<T>::getters>;
+
+namespace result_of {
+template<class T>
+    requires TupleLike<std::remove_cvref_t<T>>
+inline constexpr std::size_t size = adaptor<std::remove_cvref_t<T>>::getters::size;
+
+} // result_of
+
+template<class T>
+    requires TupleLike<std::remove_cvref_t<T>>
+constexpr std::size_t size(T&&) noexcept
 {
-    template <typename T>
-    struct adaptor;
+    return result_of::size<T>;
+}
 
-    template <typename T>
-    concept TupleLike = detail::NonTypeList<typename adaptor<T>::getters>;
+namespace result_of {
 
-    namespace result_of
-    {
-        template <typename T>
-            requires TupleLike<std::remove_cvref_t<T>>
-        inline constexpr std::size_t size = adaptor<std::remove_cvref_t<T>>::getters::size;
-    } // result_of
+// `std::invoke_result_t` MUST NOT be used here due to its side effects:
+// <https://eel.is/c++draft/meta.trans.other#tab:meta.trans.other-row-11-column-2-note-2>
+template<std::size_t I, class T>
+    requires TupleLike<std::remove_cvref_t<T>>
+using get = decltype(std::invoke(adaptor<std::remove_cvref_t<T>>::getters::template get<I>, std::declval<T>()));
+} // result_of
 
-    template <typename T>
-        requires TupleLike<std::remove_cvref_t<T>>
-    constexpr std::size_t size(T&&) noexcept
-    {
-        return result_of::size<T>;
-    }
+template<std::size_t I, class T>
+    requires TupleLike<std::remove_cvref_t<T>>
+constexpr result_of::get<I, T> get(T&& x)
+    noexcept(noexcept(std::is_nothrow_invocable_v<decltype(adaptor<std::remove_cvref_t<T>>::getters::template get<I>), T>))
+{
+    return std::invoke(adaptor<std::remove_cvref_t<T>>::getters::template get<I>, std::forward<T>(x));
+}
 
-    namespace result_of
-    {
-        // `std::invoke_result_t` MUST NOT be used here due to its side effects:
-        // <https://eel.is/c++draft/meta.trans.other#tab:meta.trans.other-row-11-column-2-note-2>
-        template <std::size_t I, typename T>
-            requires TupleLike<std::remove_cvref_t<T>>
-        using get = decltype(std::invoke(adaptor<std::remove_cvref_t<T>>::getters::template get<I>, std::declval<T>()));
-    } // result_of
-
-    template <std::size_t I, typename T>
-        requires TupleLike<std::remove_cvref_t<T>>
-    constexpr result_of::get<I, T> get(T&& x)
-        noexcept(noexcept(std::is_nothrow_invocable_v<decltype(adaptor<std::remove_cvref_t<T>>::getters::template get<I>), T>))
-    {
-        return std::invoke(adaptor<std::remove_cvref_t<T>>::getters::template get<I>, std::forward<T>(x));
-    }
 } // boost::spirit::alloy
 
 #endif
--- include/boost/spirit/alloy/tuple.hpp
+++ include/boost/spirit/alloy/tuple.hpp
@@ -16,115 +16,120 @@
 
 #include <boost/spirit/alloy/value_initialize.hpp>
 
-namespace boost::spirit::alloy
+namespace boost::spirit::alloy {
+
+template<class... Ts>
+class tuple : public detail::tuple_impl<Ts...>
 {
-    template <typename... Ts>
-    class tuple : public detail::tuple_impl<Ts...>
-    {
-    private:
-        static_assert(!std::disjunction_v<std::is_rvalue_reference<Ts>...>, "alloy::tuple must not be instantiated with rvalue reference type");
-        using base_type = detail::tuple_impl<Ts...>;
+  private:
+    static_assert(!std::disjunction_v<std::is_rvalue_reference<Ts>...>, "alloy::tuple must not be instantiated with rvalue reference type");
+    using base_type = detail::tuple_impl<Ts...>;
 
-        template <typename... Us>
-        static constexpr bool disambiguating_constraint = [](){
-            if constexpr (sizeof...(Ts) == 1) {
-              return !std::is_same_v<std::remove_cvref_t<detail::type_pack_indexing_t<0, Us...>>, tuple>;
-            } else {
-              return true;
-            }
-        }();
-
-    public:
-        tuple() = default;
-
-        tuple(tuple const&) = default;
-
-        tuple(tuple&&)
-            requires std::conjunction_v<std::is_move_constructible<Ts>...>
-        = default;
-
-        constexpr explicit tuple(value_initialize_t vi) : base_type(vi) {}
-
-        constexpr explicit(!std::conjunction_v<std::is_convertible<Ts const&, Ts>...>) tuple(Ts const&... ts)
-            noexcept(std::conjunction_v<std::is_nothrow_copy_constructible<Ts>...>)
-            requires requires {
-                requires (sizeof...(Ts) > 0);
-                requires std::conjunction_v<std::is_copy_constructible<Ts>...>;
-            }
-            : base_type(ts...) {}
-
-        template <typename... Us>
-            requires requires {
-                requires (sizeof...(Ts) == sizeof...(Us));
-                requires disambiguating_constraint<Us...>;
-                requires std::conjunction_v<std::is_constructible<Ts, Us>...>;
-            }
-        constexpr explicit(!std::conjunction_v<std::is_convertible<Us, Ts>...>) tuple(Us&&... us) noexcept(std::conjunction_v<std::is_nothrow_constructible<Ts, Us>...>)
-            : base_type(static_cast<Us&&>(us)...) {}
-
-        template <std::size_t I, typename Self>
-        constexpr detail::combine_ref_t<Self&&, detail::type_pack_indexing_t<I, Ts...>> get(this Self&& self) noexcept
-        {
-            static_assert(I < sizeof...(Ts));
-            return ((detail::forward_like_t<Self, tuple>)self).base_type::template get<I>();
+    template<class... Us>
+    static constexpr bool disambiguating_constraint = []() {
+        if constexpr (sizeof...(Ts) == 1) {
+            return !std::is_same_v<std::remove_cvref_t<detail::type_pack_indexing_t<0, Us...>>, tuple>;
+        } else {
+            return true;
         }
-    };
+    }();
 
-    template <std::size_t I, typename... Ts>
-    constexpr detail::type_pack_indexing_t<I, Ts...>& get(tuple<Ts...>& t) noexcept
+  public:
+    tuple() = default;
+
+    tuple(tuple const&) = default;
+
+    tuple(tuple&&)
+        requires std::conjunction_v<std::is_move_constructible<Ts>...>
+    = default;
+
+    constexpr explicit tuple(value_initialize_t vi) : base_type(vi) {}
+
+    constexpr explicit(!std::conjunction_v<std::is_convertible<Ts const&, Ts>...>) tuple(Ts const&... ts)
+        noexcept(std::conjunction_v<std::is_nothrow_copy_constructible<Ts>...>)
+        requires requires {
+            requires (sizeof...(Ts) > 0);
+            requires std::conjunction_v<std::is_copy_constructible<Ts>...>;
+        }
+        : base_type(ts...)
+    {}
+
+    template<class... Us>
+        requires requires {
+            requires (sizeof...(Ts) == sizeof...(Us));
+            requires disambiguating_constraint<Us...>;
+            requires std::conjunction_v<std::is_constructible<Ts, Us>...>;
+        }
+    constexpr explicit(!std::conjunction_v<std::is_convertible<Us, Ts>...>) tuple(Us&&... us)
+        noexcept(std::conjunction_v<std::is_nothrow_constructible<Ts, Us>...>)
+        : base_type(static_cast<Us&&>(us)...)
+    {}
+
+    template<std::size_t I, class Self>
+    constexpr detail::combine_ref_t<Self&&, detail::type_pack_indexing_t<I, Ts...>> get(this Self&& self) noexcept
     {
         static_assert(I < sizeof...(Ts));
-        return t.template get<I>();
+        return ((detail::forward_like_t<Self, tuple>)self).base_type::template get<I>();
     }
-    
-    template <std::size_t I, typename... Ts>
-    constexpr detail::type_pack_indexing_t<I, Ts...> const& get(tuple<Ts...> const& t) noexcept
+};
+
+template<std::size_t I, class... Ts>
+constexpr detail::type_pack_indexing_t<I, Ts...>& get(tuple<Ts...>& t) noexcept
+{
+    static_assert(I < sizeof...(Ts));
+    return t.template get<I>();
+}
+
+template<std::size_t I, class... Ts>
+constexpr detail::type_pack_indexing_t<I, Ts...> const& get(tuple<Ts...> const& t) noexcept
+{
+    static_assert(I < sizeof...(Ts));
+    return t.template get<I>();
+}
+
+template<std::size_t I, class... Ts>
+constexpr detail::type_pack_indexing_t<I, Ts...>&& get(tuple<Ts...>&& t) noexcept
+{
+    static_assert(I < sizeof...(Ts));
+    return static_cast<tuple<Ts...>&&>(t).template get<I>();
+}
+
+template<std::size_t I, class... Ts>
+constexpr detail::type_pack_indexing_t<I, Ts...> const&& get(tuple<Ts...> const&& t) noexcept
+{
+    static_assert(I < sizeof...(Ts));
+    return static_cast<tuple<Ts...> const&&>(t).template get<I>();
+}
+
+namespace detail {
+
+template<std::size_t I>
+struct call_alloy_get
+{
+    template<typename Tuple>
+    static constexpr decltype(auto) operator()(Tuple&& t) noexcept
     {
-        static_assert(I < sizeof...(Ts));
-        return t.template get<I>();
+        return alloy::get<I>(static_cast<Tuple&&>(t));
     }
-    
-    template <std::size_t I, typename... Ts>
-    constexpr detail::type_pack_indexing_t<I, Ts...>&& get(tuple<Ts...>&& t) noexcept
-    {
-        static_assert(I < sizeof...(Ts));
-        return static_cast<tuple<Ts...>&&>(t).template get<I>();
-    }
-    
-    template <std::size_t I, typename... Ts>
-    constexpr detail::type_pack_indexing_t<I, Ts...> const&& get(tuple<Ts...> const&& t) noexcept
-    {   
-        static_assert(I < sizeof...(Ts));
-        return static_cast<tuple<Ts...> const&&>(t).template get<I>();
-    }
+};
 
-    namespace detail
-    {
-        template <std::size_t I>
-        struct call_alloy_get
-        {
-            template <typename Tuple>
-            static constexpr decltype(auto) operator()(Tuple&& t) noexcept
-            {
-                return alloy::get<I>(static_cast<Tuple&&>(t));
-            }
-        };
+template<std::size_t I>
+struct make_call_alloy_get
+{
+    static constexpr auto value = call_alloy_get<I>{};
+};
 
-        template <std::size_t I>
-        struct make_call_alloy_get
-        {
-            static constexpr auto value = call_alloy_get<I>{};
-        };
-    } // detail
+} // detail
 
-    template <typename T>
-    struct adaptor;
+template<class T>
+struct adaptor;
 
-    template <typename... Ts>
-    struct adaptor<tuple<Ts...>>
-    {
-        using getters = detail::integer_seq_transform_t<std::make_index_sequence<sizeof...(Ts)>, detail::make_call_alloy_get>;
-    };
+template<class... Ts>
+struct adaptor<tuple<Ts...>>
+{
+    using getters = detail::integer_seq_transform_t<std::make_index_sequence<sizeof...(Ts)>, detail::make_call_alloy_get>;
+};
+
 } // boost::spirit::alloy
 
 #endif
