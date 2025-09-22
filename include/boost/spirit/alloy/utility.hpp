19d65865e3144a85abba1dc832f8dc784c5f7f7c
yaito3014 <ykakeyama3014@gmail.com>
2025-09-22 16:48:54 +0900

    Implement tuple_split

--- include/boost/spirit/alloy/utility.hpp
+++ include/boost/spirit/alloy/utility.hpp
@@ -91,14 +91,170 @@ struct tuple_cat_impl<ResultTuple, type_list<std::index_sequence<Is...>, IndexSe
     }
 };
 
+template<std::size_t N, class TIndexSeq, class UIndexSeq>
+struct index_sequence_split_impl;
+
+template<std::size_t N, std::size_t... Is, std::size_t J, std::size_t... Js>
+    requires (sizeof...(Is) < N)
+struct index_sequence_split_impl<N, std::index_sequence<Is...>, std::index_sequence<J, Js...>>
+    : index_sequence_split_impl<N, std::index_sequence<Is..., J>, std::index_sequence<Js...>> {};
+
+template<std::size_t N, std::size_t... Is, std::size_t... Js>
+    requires (sizeof...(Is) == N)
+struct index_sequence_split_impl<N, std::index_sequence<Is...>, std::index_sequence<Js...>>
+{
+    using head = std::index_sequence<Is...>;
+    using tail = std::index_sequence<Js...>;
+};
+
+template<std::size_t N, class IndexSeq>
+struct index_sequence_split : index_sequence_split_impl<N, std::index_sequence<>, IndexSeq> {};
+
+template<std::size_t N, class IndexSeq>
+struct index_sequence_take
+{
+    using type = typename index_sequence_split<N, IndexSeq>::head;
+};
+
+template<std::size_t N, class IndexSeq>
+using index_sequence_take_t = typename index_sequence_take<N, IndexSeq>::type;
+
+template<std::size_t N, class IndexSeq>
+struct index_sequence_drop
+{
+    using type = typename index_sequence_split<N, IndexSeq>::tail;
+};
+
+template<std::size_t N, class IndexSeq>
+using index_sequence_drop_t = typename index_sequence_drop<N, IndexSeq>::type;
+
+template<std::size_t From, std::size_t To, class IndexSeq>
+struct index_sequence_subrange
+{
+    using type = index_sequence_take_t<To - From, index_sequence_drop_t<From, IndexSeq>>;
+};
+
+template<std::size_t From, std::size_t To, class IndexSeq>
+using index_sequence_subrange_t = typename index_sequence_subrange<From, To, IndexSeq>::type;
+
+template<class IndexSeq>
+struct index_sequence_sum;
+
+template<std::size_t... Is>
+struct index_sequence_sum<std::index_sequence<Is...>> : std::integral_constant<std::size_t, (0 + ... + Is)> {};
+
+template<class IndexSeq>
+inline constexpr std::size_t index_sequence_sum_v = index_sequence_sum<IndexSeq>::value;
+
+template<class IdxIndexSeq, class ValIndexSeq>
+struct index_sequence_cumulative_sum_impl;
+
+template<std::size_t... Idxs, class ValIndexSeq>
+struct index_sequence_cumulative_sum_impl<std::index_sequence<Idxs...>, ValIndexSeq>
+{
+    using type = std::index_sequence<0, index_sequence_sum_v<index_sequence_take_t<Idxs + 1, ValIndexSeq>>...>;
+};
+
+template<class IndexSeq>
+struct index_sequence_cumulative_sum;
+
+template<std::size_t... Is>
+struct index_sequence_cumulative_sum<std::index_sequence<Is...>>
+    : index_sequence_cumulative_sum_impl<std::make_index_sequence<sizeof...(Is)>, std::index_sequence<Is...>> {};
+
+template<class IndexSeq>
+using index_sequence_cumulative_sum_t = typename index_sequence_cumulative_sum<IndexSeq>::type;
+
+template<class Tuple, class IndexSeq>
+struct tuple_from_tuple_and_index_sequence;
+
+template<class Tuple, std::size_t... Is>
+struct tuple_from_tuple_and_index_sequence<Tuple, std::index_sequence<Is...>>
+{
+    using type = tuple<tuple_deduce_t<Is, Tuple>...>;
+};
+
+template<class Tuple, class IndexSeq>
+using tuple_from_tuple_and_index_sequence_t = typename tuple_from_tuple_and_index_sequence<Tuple, IndexSeq>::type;
+
+template<class Tuple, class FromIndexSeq, class ToIndexSeq>
+struct tuple_split_result_impl;
+
+template<class Tuple, std::size_t... Froms, std::size_t... Tos>
+struct tuple_split_result_impl<Tuple, std::index_sequence<Froms...>, std::index_sequence<Tos...>>
+{
+    using type = tuple<tuple_from_tuple_and_index_sequence_t<Tuple, index_sequence_subrange_t<Froms, Tos, std::make_index_sequence<result_of::size<Tuple>>>>...>;
+};
+
+template<class Tuple, std::size_t... Sizes>
+struct tuple_split_result
+{
+    using CumSumIndexSeq = index_sequence_cumulative_sum_t<std::index_sequence<Sizes...>>;
+    using type = typename tuple_split_result_impl<Tuple, index_sequence_take_t<result_of::size<Tuple>, CumSumIndexSeq>, index_sequence_drop_t<1, CumSumIndexSeq>>::type;
+};
+
+template<class Tuple, std::size_t... Sizes>
+using tuple_split_result_t = typename tuple_split_result<Tuple, Sizes...>::type;
+
+template<class IndexSeq>
+struct tuple_split_make_inner;
+
+template<std::size_t... Is>
+struct tuple_split_make_inner<std::index_sequence<Is...>>
+{
+    template<class Tuple>
+    using ResultInnerTuple = tuple_from_tuple_and_index_sequence_t<Tuple, std::index_sequence<Is...>>;
+
+    template<class Tuple>
+    static constexpr ResultInnerTuple<Tuple> apply(Tuple&& t)
+    {
+        return ResultInnerTuple<Tuple> (alloy::get<Is>(std::forward<Tuple>(t))...);
+    }
+};
+
+template<class ResultTuple, class... IndexSeqs>
+struct tuple_split_make_outer;
+
+template<class... ResultInnerTuples, class... IndexSeqs>
+struct tuple_split_make_outer<tuple<ResultInnerTuples...>, IndexSeqs...>
+{
+    template<class Tuple>
+    static constexpr tuple<ResultInnerTuples...> apply(Tuple&& t)
+    {
+        return tuple<ResultInnerTuples...>(tuple_split_make_inner<IndexSeqs>::apply(std::forward<Tuple>(t))...);
+    }
+};
+
+template<class ResultTuple, class IdxIndexSeq, class FromIndexSeq, class ToIndexSeq>
+struct tuple_split_impl;
+
+template<class ResultTuple, class IdxIndexSeq, std::size_t... Froms, std::size_t... Tos>
+struct tuple_split_impl<ResultTuple, IdxIndexSeq, std::index_sequence<Froms...>, std::index_sequence<Tos...>>
+    : tuple_split_make_outer<ResultTuple, index_sequence_subrange_t<Froms, Tos, IdxIndexSeq>...> {};
+
 } // detail
 
 template<class... Tuples>
     requires (TupleLike<std::remove_cvref_t<Tuples>> && ...)
 constexpr detail::tuple_cat_result_t<Tuples...> tuple_cat(Tuples&&... tuples)
 {
-    using impl = detail::tuple_cat_impl<detail::tuple_cat_result_t<Tuples...>, detail::type_list<std::make_index_sequence<result_of::size<Tuples>>...>, Tuples...>;
-    return impl::apply(std::forward<Tuples>(tuples)...);
+    using Impl = detail::tuple_cat_impl<detail::tuple_cat_result_t<Tuples...>, detail::type_list<std::make_index_sequence<result_of::size<Tuples>>...>, Tuples...>;
+    return Impl::apply(std::forward<Tuples>(tuples)...);
+}
+
+template<std::size_t... Sizes, class Tuple>
+    requires TupleLike<std::remove_cvref_t<Tuple>>
+constexpr detail::tuple_split_result_t<Tuple, Sizes...> tuple_split(Tuple&& t)
+{
+    static_assert((0 + ... + Sizes) == result_of::size<Tuple>);
+    using CumSumIndexSeq = detail::index_sequence_cumulative_sum_t<std::index_sequence<Sizes...>>;
+    using Impl = detail::tuple_split_impl<
+        detail::tuple_split_result_t<Tuple, Sizes...>,
+        std::make_index_sequence<result_of::size<Tuple>>,
+        detail::index_sequence_take_t<result_of::size<Tuple>, CumSumIndexSeq>,
+        detail::index_sequence_drop_t<1, CumSumIndexSeq>
+    >;
+    return Impl::apply(std::forward<Tuple>(t));
 }
 
 } // boost::spirit::alloy
