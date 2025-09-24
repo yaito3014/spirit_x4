86af30851734ba6d8caa991c079c90d96e0324f8
yaito3014 <ykakeyama3014@gmail.com>
2025-09-24 16:10:49 +0900

    Add copyright

--- include/boost/spirit/alloy/tuple_like_view.hpp
+++ include/boost/spirit/alloy/tuple_like_view.hpp
@@ -1,6 +1,13 @@
 #ifndef BOOST_SPIRIT_ALLOY_TUPLE_LIKE_VIEW_HPP
 #define BOOST_SPIRIT_ALLOY_TUPLE_LIKE_VIEW_HPP
 
+/*=============================================================================
+    Copyright (c) 2025 Yaito Kakeyama
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
 #include <boost/spirit/alloy/detail/deduce.hpp>
 
 #include <boost/spirit/alloy/tuple_like.hpp>
