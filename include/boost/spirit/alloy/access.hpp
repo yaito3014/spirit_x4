9847cfa3eabd575a558cc425899e66bf19f397a1
yaito3014 <ykakeyama3014@gmail.com>
2025-09-24 17:14:46 +0900

    Apply workaround for Clang

--- include/boost/spirit/alloy/access.hpp
+++ include/boost/spirit/alloy/access.hpp
@@ -33,21 +33,30 @@ constexpr std::size_t size(T&&) noexcept
     return result_of::size<T>;
 }
 
+namespace detail {
+
+// Clang workaround
+template<std::size_t I, class T>
+inline constexpr auto getter_of = adaptor<T>::getters::template get<I>;
+
+} // detail
+
 namespace result_of {
 
 // `std::invoke_result_t` MUST NOT be used here due to its side effects:
 // <https://eel.is/c++draft/meta.trans.other#tab:meta.trans.other-row-11-column-2-note-2>
 template<std::size_t I, class T>
     requires TupleLike<std::remove_cvref_t<T>>
-using get = decltype(std::invoke(adaptor<std::remove_cvref_t<T>>::getters::template get<I>, std::declval<T>()));
+using get = decltype(std::invoke(detail::getter_of<I, std::remove_cvref_t<T>>, std::declval<T>()));
+
 } // result_of
 
 template<std::size_t I, class T>
     requires TupleLike<std::remove_cvref_t<T>>
 constexpr result_of::get<I, T> get(T&& x)
-    noexcept(noexcept(std::is_nothrow_invocable_v<decltype(adaptor<std::remove_cvref_t<T>>::getters::template get<I>), T>))
+    noexcept(noexcept(std::is_nothrow_invocable_v<decltype(detail::getter_of<I, std::remove_cvref_t<T>>), T>))
 {
-    return std::invoke(adaptor<std::remove_cvref_t<T>>::getters::template get<I>, std::forward<T>(x));
+    return std::invoke(detail::getter_of<I, std::remove_cvref_t<T>>, std::forward<T>(x));
 }
 
 } // boost::spirit::alloy
