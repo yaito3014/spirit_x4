c72dc0a958306db30d4ea44a5c2a2c4366c85e7e
yaito3014 <ykakeyama3014@gmail.com>
2025-09-24 16:10:49 +0900

    Add copyright

--- include/boost/spirit/alloy/tuple_like.hpp
+++ include/boost/spirit/alloy/tuple_like.hpp
@@ -1,6 +1,13 @@
 #ifndef BOOST_SPIRIT_ALLOY_TUPLE_LIKE_HPP
 #define BOOST_SPIRIT_ALLOY_TUPLE_LIKE_HPP
 
+/*=============================================================================
+    Copyright (c) 2025 Yaito Kakeyama
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
 #include <boost/spirit/alloy/non_type_list.hpp>
 
 #include <type_traits>
