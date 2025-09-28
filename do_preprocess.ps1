96b1d6797c956a59de174fb3530aa375ca09b247
yaito3014 <ykakeyama3014@gmail.com>
2025-09-28 14:48:38 +0900

    Remove ps1

--- do_preprocess.ps1
+++
@@ -1,6 +0,0 @@
-cl.exe /EP /Iinclude /I..\preprocessor\include /DBOOST_SPIRIT_ALLOY_GENERATE_PREPROCESSED include/boost/spirit/alloy/detail/tuple_impl.hpp > include/boost/spirit/alloy/detail/preprocessed/temp.hpp
-Push-Location include/boost/spirit/alloy/detail/preprocessed
-(Get-Content tuple_impl.hpp.in -Raw) -replace '// replace me', $(Get-Content temp.hpp -Raw) | Set-Content tuple_impl.hpp
-clang-format -i tuple_impl.hpp
-Remove-Item temp.hpp
-Pop-Location
